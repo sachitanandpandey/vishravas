31a42b531efad26fcca93c128f73ca5e3c072b57
Sachitanand <sachitanand.pandey@gmail.com>
2022-10-03 07:56:43 +0100

    Update Footer.vue

--- vishravas/src/components/Footer.vue
+++ vishravas/src/components/Footer.vue
@@ -1,21 +1,37 @@
 <template>
     <v-container>
       <v-footer padless>
-        <v-card flat class="text-center">
+        <v-card class="flex flat tile">
+        <v-card-title class="primary lighten-1">
+          <v-layout class="flex justify-center grow">
+            <strong class="subheading white--text">
+              Some Random Text
+            </strong>
+          </v-layout>
+          <v-checkbox dark label="Click Me" class="shrink"></v-checkbox>
+          <v-btn v-on:click="saveClicked">
+            <v-icon>save</v-icon> Save
+          </v-btn>
+          <v-btn>
+            <v-icon>close</v-icon> Cancel
+          </v-btn>
+        </v-card-title>
+      </v-card>
+        <!-- <v-card flat class="text-center">
           <v-card-text>
             <v-btn v-for="icon in icons" :key="icon" class="mx-4" icon>
               <v-icon size="24px">{{ icon }}</v-icon>
             </v-btn>
           </v-card-text>
           <v-card-text
-            class="pt-0"
-          >Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</v-card-text>
+            class="pt-20"
+          >Test</v-card-text>
           <v-divider></v-divider>
           <v-card-text>
             {{ new Date().getFullYear() }} —
             <strong>Photography Vuetify Project</strong>
           </v-card-text>
-        </v-card>
+        </v-card> -->
       </v-footer>
     </v-container>
   </template>
