d8f72696d74a7c3a9be1381b81d3d301224219f1
Sachitanand <sachitanand.pandey@gmail.com>
2022-12-19 12:45:52 +0000

    adding initial casting page
    adding initial casting page

--- vishravas/src/components/castingHome.vue
+++ vishravas/src/components/castingHome.vue
@@ -16,95 +16,86 @@ export default Vue.extend({
 </script> -->
 
 <template>
-    <v-app id="app">
-        <v-content>
-            <v-container fluid fill-height>
-                <v-row class="pa-md-6">
-                    <v-card color="#385F73" max-width="100%" height="100%"
-                        class="d-flex justify-space-around mb-6 center">
-                        <v-container>
-                            <v-row dense>
-                                <v-col cols="20">
-                                    <v-container>
-                                        <h1>Audition</h1>
-                                        <v-card max-width="800px" height="500px" class="mt-2 mb-2">
-                                            <v-container>
-      <v-row dense>
-        <v-col cols="12">
-          <v-card
-            color="#385F73"
-            dark
-          >
-            <v-card-title class="text-h4">
-              Page is under construction
-            </v-card-title>
-            <!--
-            <v-card-subtitle>Listen to your favorite artists and albums whenever and wherever, online and offline.</v-card-subtitle>
+  <v-app id="app">
+    <v-content>
+      <v-container fluid fill-height>
+        <v-row class="pa-md-6">
+          <v-card color="#385F73" max-width="100%" height="100%" class="d-flex justify-space-around mb-6 center">
+            <v-container>
+              <v-row dense>
+                <v-col cols="20">
+                  <v-container>
+                    <h1>Audition</h1>
+                    <v-card max-width="800px" height="100%" class="mt-2 mb-2">
+                      <v-container>
+                        <v-row dense>
+                          <v-col cols="12">
+                            <v-card color="#385F73" dark>
+                              <div v-for="item in data.doclist[0].casting" v-bind:key="item.id">
+                                <v-card color="" class="mt-2 ml-2 mr-2 mb-4">
+                                  <v-row>
+                                    <v-col>
+                                      <v-col class="d-flex" cols="12" sm="12">
+                                        <v-row>
+                                          <v-col>
+                                            <v-col class="d-flex" cols="12" sm="12">
+                                              {{item.name}}
+                                            </v-col>
+                                            <v-col class="d-flex" cols="12" sm="12">
+                                              {{item.age}}
+                                            </v-col>
+                                            <v-col class="d-flex" cols="12" sm="12">
+                                              {{item.description}}
+                                            </v-col>
+                                            <v-col class="d-flex" cols="12" sm="12">
+                                              {{item.ethnicity}}
+                                            </v-col>
+                                          </v-col>
+                                        </v-row>
 
-            <v-card-actions>
-              <v-btn text>
-                Listen Now
-              </v-btn>
-            </v-card-actions> -->
-          </v-card>
-        </v-col>
+                                        <v-card-actions>
+                                          <v-btn rounded color="primary" class="mt-2 ml-2 mr-2 mb-4 d-flex justify-end" dark @click="update()">
+                                            Apply
+                                          </v-btn>
+                                        </v-card-actions>
+                                      </v-col>
+                                    </v-col>
+                                  </v-row>
+                                </v-card>
+                              </div>
 
-        <v-col
-          v-for="(item, i) in items"
-          :key="i"
-          cols="12"
-        >
-          <v-card
-            :color="item.color"
-            dark
-          >
-            <div class="d-flex flex-no-wrap justify-space-between">
-              <div>
-                <v-card-title
-                  class="text-h5"
-                  v-text="item.title"
-                ></v-card-title>
+                            </v-card>
+                          </v-col>
 
-                <v-card-subtitle v-text="item.artist"></v-card-subtitle>
+                          <v-col v-for="(item, i) in items" :key="i" cols="12">
+                            <v-card :color="item.color" dark>
+                              <div class="d-flex flex-no-wrap justify-space-between">
+                                <div>
+                                  <v-card-title class="text-h5" v-text="item.title"></v-card-title>
 
-                <v-card-actions>
-                  <v-btn
-                    v-if="item.artist === 'Ellie Goulding'"
-                    class="ml-2 mt-3"
-                    fab
-                    icon
-                    height="40px"
-                    right
-                    width="40px"
-                  >
-                    <v-icon>mdi-play</v-icon>
-                  </v-btn>
+                                  <v-card-subtitle v-text="item.artist"></v-card-subtitle>
 
-                  <v-btn
-                    v-else
-                    class="ml-2 mt-5"
-                    outlined
-                    rounded
-                    small
-                  >
-                    START RADIO
-                  </v-btn>
-                </v-card-actions>
-              </div>
+                                  <v-card-actions>
+                                    <v-btn v-if="item.artist === 'Ellie Goulding'" class="ml-2 mt-3" fab icon
+                                      height="40px" right width="40px">
+                                      <v-icon>mdi-play</v-icon>
+                                    </v-btn>
 
-              <v-avatar
-                class="ma-3"
-                size="125"
-                tile
-              >
-                <v-img :src="item.src"></v-img>
-              </v-avatar>
-            </div>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-                                            <!-- <form @submit.prevent="submit">
+                                    <v-btn v-else class="ml-2 mt-5" outlined rounded small>
+                                      START RADIO
+                                    </v-btn>
+                                  </v-card-actions>
+                                </div>
+
+                                <v-avatar class="ma-3" size="125" tile>
+                                  <v-img :src="item.src"></v-img>
+                                </v-avatar>
+                              </div>
+                            </v-card>
+                          </v-col>
+                        </v-row>
+                      </v-container>
+                      <!-- <form @submit.prevent="submit">
                                                 <v-row>
                                                     <v-col>
                                                         <v-col class="d-flex" cols="12" sm="12">
@@ -172,67 +163,71 @@ export default Vue.extend({
                                                 @click="update()">
                                                 Update
                                             </v-btn> -->
-                                        </v-card>
-                                    </v-container>
-                                </v-col>
-                            </v-row>
-                        </v-container>
                     </v-card>
-                    <v-row>
-                        <template>
-                            <v-card max-width="400" max-height="630px" class="mx-auto">
-                                <v-container>
-                                    <v-row dense>
-                                        <v-col cols="12">
-                                            <v-card max-width="100%" height="100%">
-                                                <v-img src='../assets/ComingSoon.jpg' class="white--text align-end"
-                                                    gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)" height="100%"
-                                                    width="100%">
-                                                    <v-card-actions>
-                                                        <v-btn rounded color="primary" dark @click="replace(item)">
-                                                            Edit
-                                                        </v-btn>
-                                                    </v-card-actions>
-                                                </v-img>
-                                            </v-card>
-                                        </v-col>
-                                    </v-row>
-                                </v-container>
-                            </v-card>
-                        </template>
-                    </v-row>
-                        <v-layout align-top justify-end>
-                            <v-flex xs20 sm8 md4>
-                                <v-row>
-                                    <v-col xs="6" class="d-flex justify-end">
-                                            <v-btn rounded color="primary" dark @click="home()">
-                                                Home
-                                            </v-btn>
-                                    </v-col>
-                                </v-row>
-                            </v-flex>
-                        </v-layout>
-                </v-row>
-                <v-container>
-                    <v-layout align-top justify-end>
-                        <v-flex xs20 sm8 md4>
-                            <v-row>
-                                <v-col xs="6" class="d-flex justify-end">
-                                    <form @submit.prevent="submit">
-                                        <v-btn rounded color="primary" dark type="submit">
-                                            SignOut
-                                        </v-btn>
-                                    </form>
-                                </v-col>
-                            </v-row>
-                        </v-flex>
-                    </v-layout>
-                </v-container>
+                  </v-container>
+                </v-col>
+              </v-row>
             </v-container>
+          </v-card>
+          <v-row>
+            <template>
+              <v-card max-width="400" max-height="630px" class="mx-auto">
+                <v-container>
+                  <v-row dense>
+                    <v-col cols="12">
+                      <v-card max-width="100%" height="100%">
+                        <div v-if="data.doclist[0].audition">
+                          <v-img :src="data.doclist[0].audition" class="white--text align-end"
+                            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)" height="100%" width="100%">
 
-        </v-content>
+                          </v-img>
+                        </div>
+                        <div v-else>
+                          <v-img src='../assets/ComingSoon.jpg' class="white--text align-end"
+                            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)" height="100%" width="100%">
 
-    </v-app>
+                          </v-img>
+                        </div>
+
+                      </v-card>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card>
+            </template>
+          </v-row>
+          <v-layout align-top justify-end>
+            <v-flex xs20 sm8 md4>
+              <v-row>
+                <v-col xs="6" class="d-flex justify-end">
+                  <v-btn rounded color="primary" dark @click="home()">
+                    Home
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-flex>
+          </v-layout>
+        </v-row>
+        <v-container>
+          <v-layout align-top justify-end>
+            <v-flex xs20 sm8 md4>
+              <v-row>
+                <v-col xs="6" class="d-flex justify-end">
+                  <form @submit.prevent="submit">
+                    <v-btn rounded color="primary" dark type="submit">
+                      SignOut
+                    </v-btn>
+                  </form>
+                </v-col>
+              </v-row>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-container>
+
+    </v-content>
+
+  </v-app>
 </template>
 
 <script>
@@ -248,7 +243,7 @@ export default {
   setup () {
     const data = reactive({
       //   submit: '',
-      //   doclist: '',
+      doclist: ''
       //   premierlist: '',
       //   audilist: '',
       //   display: ''
@@ -258,19 +253,20 @@ export default {
     const GenderOptions = ['Male', 'Female']
     const weekendOptions = ['Yes', 'No']
 
-    // onMounted(async () => {
-    //   const qDoclist = query(collection(db, 'projects'), where('status', '==', 'Projected'))
-    //   const qPremierlist = query(collection(db, 'projects'), where('status', '==', 'Premiering'))
-    //   const qProgresslist = query(collection(db, 'projects'), where('status', '==', 'InProgress'))
+    onMounted(async () => {
+      const qDoclist = query(collection(db, 'casting'), where('status', '==', 'open'))
+      //   const qPremierlist = query(collection(db, 'projects'), where('status', '==', 'Premiering'))
+      //   const qProgresslist = query(collection(db, 'projects'), where('status', '==', 'InProgress'))
 
-    //   const querySnapshot = await getDocs(qDoclist)
-    //   data.doclist = querySnapshot.docs.map(doc => doc.data())
-    //   const queryPlist = await getDocs(qPremierlist)
-    //   data.premierlist = queryPlist.docs.map(doc => doc.data())
-    //   data.display = data.premierlist[0]
-    //   const queryFlist = await getDocs(qProgresslist)
-    //   data.audilist = queryFlist.docs.map(doc => doc.data())
-    // })
+      const querySnapshot = await getDocs(qDoclist)
+      data.doclist = querySnapshot.docs.map(doc => doc.data())
+      console.log(data.doclist[0])
+      //   const queryPlist = await getDocs(qPremierlist)
+      //   data.premierlist = queryPlist.docs.map(doc => doc.data())
+      //   data.display = data.premierlist[0]
+      //   const queryFlist = await getDocs(qProgresslist)
+      //   data.audilist = queryFlist.docs.map(doc => doc.data())
+    })
 
     onAuthStateChanged(auth, (user) => {
       if (user) {
